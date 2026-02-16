3fc36c0ef7062f48e07d88354b71236805f2fe73
Anas Ghouila <ghouilaa.anas@gmail.com>
2026-02-16 08:43:06 +0100

    Update main.h

--- STM32 Projects/Raw_Register_Access_STM32/Inc/main.h
+++ STM32 Projects/Raw_Register_Access_STM32/Inc/main.h
@@ -7,16 +7,15 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define RCC                     (*(volatile uint32_t*)0x40021000)
 #define RCC_APB2ENR             (*(volatile uint32_t*) 0x40021018)
 #define GPIOA                   (*(volatile uint32_t*)0x40010800)
 #define GPIOA_ODR               (*(volatile uint32_t*)0x4001080C)
 #define GPIOA_IDR               (*(volatile uint32_t*)0x40010808)
-#define GPIOA_MODER             (*(volatile uint32_t*)0x40010800)
+#define GPIOA_CRL               (*(volatile uint32_t*)0x40010800)
 #define RCC_CFGR                (*(volatile uint32_t*)0x40021004)
 #define FLASH_ACR               (*(volatile uint32_t*)0x40022000)
 #define RCC_CR                  (*(volatile uint32_t*)0x40021000)
 #define RCC_APB2ENR_GPIOAEN     (1<<2)
-#define RELAY_PIN_POS            7
+#define RELAY_PIN_POS            7U
 
 #endif /* MAIN_H_ */
